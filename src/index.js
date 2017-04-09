3f0d97dcad95fce898e67e45974d70cfc4f6e8f4
Summer Guan <summerguan@Summers-MacBook-Pro.local>
2017-04-09 12:08:56 -0400

    add route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root')
+// );
+
+
+
+
+import React, { Component} from 'react';
+import { render } from 'react-dom';
+import {Router,Route,Link, hashHistory} from 'react-router'
+import App from './App'
+
+
+const root = document.getElementById('root');
+
+const routes=(
+  <Router history={ hashHistory}>
+    <Route path='/' component={App}/>
+     
+  </Router>
+
+)
+
+render (routes,root);
